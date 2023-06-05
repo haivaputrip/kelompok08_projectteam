e38d53b6ae05318156fdc36afc7af9d484e919e5
haiva putri <haiva.putri@student.uns.ac.id>
2023-06-05 17:12:59 +0700

    dikerjakan bersama-sama

---
+++ Input.py
@@ -0,0 +1,22 @@
+def datapemohon() :
+    namapemohon = str(input("masukkan nama pemohon = "))
+    keperluan = str(input("masukkan keperluan pemohon = \n (Exp:TTD proposal) \n"))
+    kegiatan = str(input("masukkan nama kegiatan = "))
+    waktukegiatan= input("masukkan tanggal kegiatan = ")
+    deadlinettd = input("masukkan deadline tanda tangan = ")
+datapemohon()
+import filepdf
+
+
+
+
+
+
+
+
+
+
+
+
+
+
