0aad5f0c0d022e19f52143f8bc29c92c8ca53cce
deopermana <deoputrapermana@gmail.com>
2023-06-12 18:52:04 +0700

    push

--- 12_06_2023/programTU.py
+++ 12_06_2023/programTU.py
@@ -6,28 +6,35 @@ print ("3. keluar")
 pilihan = int(input("masukkan pilihan anda (1-3): "))
 
 if pilihan == 1:
+    import login_tuti
+
     print ("selamat datang di TUTI")
     print ("pilih program selanjutnya")
     print ("1. Masukkan pengajuan tanda tangan")
     print ("2. konfirmasi berkas yang sudah ditanda tangan")
-    print ("3. keluar")
+    print ("3. Melanjutkan pengajuan ke pihak Fakultas")
+    print ("4. keluar")
 
     pilihan = int(input("masukkan pilihan anda (1-3): "))
 
     if pilihan == 1:
-        open("coba3.txt","r")
+        import coba3
 
     elif pilihan == 2:
         import akun_kaprodi
 
     elif pilihan == 3:
+        import akun_fakultas
+
+    elif pilihan == 4:
         print ("Terima Kasih")
 
     else:
         print("angka yang anda masukkan tidak valid")
 
 elif pilihan == 2:
-    import akun_fakultas
+    import login_fakultas
+    import fakultas
 
 elif pilihan == 3:
     print ("Terima Kasih")
